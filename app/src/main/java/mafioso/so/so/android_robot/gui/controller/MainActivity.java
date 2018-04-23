c438aa809b039f8ee9b505a7e65d21e7f3400682
mngdnmw <mtdunmow@gmail.com>
2018-04-23 12:07:20 +0200

    Removed Daniel\'s shit and added comments

--- app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
+++ app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
@@ -1,11 +1,10 @@
-package mafioso.so.so.android_robot;
+package mafioso.so.so.android_robot.gui.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -17,76 +16,70 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import mafioso.so.so.android_robot.R;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Connection_to_OpenCv";
-    private DataInputStream dis;
     private DataOutputStream dos;
+    private DataInputStream dis;
     private Socket socket = new Socket();
 
-    private EditText txtIP;
-    private TextView txtMessage;
-    private Button btnConnect;
-    private String host;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Start OpenCV
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mCallBack);
+
+
+    }
+
+    protected void loadUI(){
+
+        final TextView txtIP = findViewById(R.id.txtIP);
+        txtIP.setText("192.168.43.208");
+        Button btnConnect = findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                threadConnection(txtIP.getText().toString());
+
+            }
+        });
+
+    }
 
     BaseLoaderCallback mCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
-                case BaseLoaderCallback.SUCCESS:
-                {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
                     break;
                 }
-                default:
-                {
+                default: {
                     super.onManagerConnected(status);
                     break;
                 }
             }
         }
     };
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this,mCallBack); // Start OpenCV
-
-        txtIP = findViewById(R.id.txtIP);
-        txtIP.setText("192.168.43.174");
-        btnConnect = findViewById(R.id.btnConnect);
-        btnConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                threadConnection( txtIP.getText().toString());
-
-            }
-        });
-    }
-
-    private void sendCommand(String command) {
-        try {
-
-            dos.writeUTF(command);dos.flush();
 
 
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-    private void threadConnection(String ip){
-        host = ip;
-        new Thread(){
+    /*
+    * Connecting to robot in new thread.
+    */
+    private void threadConnection(final String host) {
+        new Thread() {
             public void run() {
                 try {
-                    if(!socket.isConnected()) {
+                    if (!socket.isConnected()) {
                         socket = new Socket(host, 5969);
-//			Socket socket = new Socket("127.0.0.1", 19231);//for mocking
                         dis = new DataInputStream(socket.getInputStream());
                         dos = new DataOutputStream(socket.getOutputStream());
                     }
                     sendCommand("We Are Here");
-                    //pw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                 } catch (UnknownHostException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -95,5 +88,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }.start();
+    }
+
+    /**
+     * Sending stuff to the robot.
+     * */
+    private void sendCommand(String command) {
+        try {
+
+            dos.writeUTF(command);
+            dos.flush();
+
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
