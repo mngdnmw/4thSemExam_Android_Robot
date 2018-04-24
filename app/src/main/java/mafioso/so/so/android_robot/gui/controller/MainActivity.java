1dda452fafd440c9e5bc90578a7b3fbe1edc4e21
jbitsme <jonas_husted@live.dk>
2018-04-24 10:12:22 +0200

    camera working(sortOfWorking)

--- app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
+++ app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
@@ -3,12 +3,22 @@ package mafioso.so.so.android_robot.gui.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -18,24 +28,101 @@ import java.net.UnknownHostException;
 
 import mafioso.so.so.android_robot.R;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "Connection_to_OpenCv";
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean mIsJavaCamera = true;
+    private MenuItem mItemSwitchCamera = null;
     private DataOutputStream dos;
     private DataInputStream dis;
     private Socket socket = new Socket();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    Mat mRgba;
+    Mat mRgbaF;
+    Mat mRgbaT;
 
-        // Start OpenCV
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mCallBack);
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "Opencv loaded successfully");
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
 
-
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new" + this.getClass());
     }
 
-    protected void loadUI(){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = findViewById(R.id.javaCameraView);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if(mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "Internal OpenCV libary not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV lib found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if(mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Core.transpose(mRgba, mRgbaT);
+        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(),0,0, 0);
+        Core.flip(mRgbaF, mRgba, 1);
+
+        return mRgba;
+    }
+
+    protected void loadUI() {
 
         final TextView txtIP = findViewById(R.id.txtIP);
         txtIP.setText("192.168.43.208");
@@ -49,24 +136,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-    BaseLoaderCallback mCallBack = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case BaseLoaderCallback.SUCCESS: {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    break;
-                }
-                default: {
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-        }
-    };
-
-
+    
     /*
     * Connecting to robot in new thread.
     */
@@ -92,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Sending stuff to the robot.
-     * */
+     */
     private void sendCommand(String command) {
         try {
 
