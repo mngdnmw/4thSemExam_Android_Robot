f9bb780c892e4aa954a376ba55749ddf5e782513
mngdnmw <mtdunmow@gmail.com>
2018-04-25 10:13:58 +0200

    Can now detect a red ball

--- app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
+++ app/src/main/java/mafioso/so/so/android_robot/gui/controller/MainActivity.java
@@ -3,8 +3,6 @@ package mafioso.so.so.android_robot.gui.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
@@ -18,6 +16,9 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.DataInputStream;
@@ -25,21 +26,29 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import mafioso.so.so.android_robot.R;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    private static final String TAG = "OCVSample::Activity";
-    private CameraBridgeViewBase mOpenCvCameraView;
-    private boolean mIsJavaCamera = true;
-    private MenuItem mItemSwitchCamera = null;
+
     private DataOutputStream dos;
     private DataInputStream dis;
     private Socket socket = new Socket();
 
-    Mat mRgba;
-    Mat mRgbaF;
-    Mat mRgbaT;
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private Mat mRgba;
+    private Mat mIntermediateMat;
+    private Mat mGray;
+    private Mat mHSV;
+    private Mat mThresholded;
+    private Mat mThresholded2;
+    private Mat array255;
+    private Mat distance;
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -76,19 +85,18 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
+
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
-        if(mOpenCvCameraView != null)
+        if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
-        if(!OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV libary not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
         } else {
@@ -99,28 +107,80 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     public void onDestroy() {
         super.onDestroy();
-        if(mOpenCvCameraView != null)
+        if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+        mHSV = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
+        array255 = new Mat(height, width, CvType.CV_8UC1);
+        distance = new Mat(height, width, CvType.CV_8UC1);
+        mThresholded = new Mat(height, width, CvType.CV_8UC1);
+        mThresholded2 = new Mat(height, width, CvType.CV_8UC1);
+
     }
 
     public void onCameraViewStopped() {
         mRgba.release();
+        mGray.release();
+        mIntermediateMat.release();
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        Core.transpose(mRgba, mRgbaT);
-        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(),0,0, 0);
-        Core.flip(mRgbaF, mRgba, 1);
 
+
+        //RBGA
+        mRgba = inputFrame.rgba();
+        //HVS
+        List<Mat> lhsv = new ArrayList<>(3);
+        Mat circles = new Mat();
+        array255.setTo(new Scalar(255));
+        Scalar hsv_min = new Scalar(0, 50, 50, 0);
+        Scalar hsv_max = new Scalar(6, 255, 255, 0);
+        Scalar hsv_min2 = new Scalar(175, 50, 50, 0);
+        Scalar hsv_max2 = new Scalar(179, 255, 255, 0);
+        // One way to select a range of colors by Hue
+        Imgproc.cvtColor(mRgba, mHSV, Imgproc.COLOR_RGB2HSV, 4);
+
+        Core.inRange(mHSV, hsv_min, hsv_max, mThresholded);
+        Core.inRange(mHSV, hsv_min2, hsv_max2, mThresholded2);
+        Core.bitwise_or(mThresholded, mThresholded2, mThresholded);
+
+        Core.split(mHSV, lhsv); // We get 3 2D one channel Mats
+        Mat S = lhsv.get(1);
+        Mat V = lhsv.get(2);
+        Core.subtract(array255, S, S);
+        Core.subtract(array255, V, V);
+        S.convertTo(S, CvType.CV_32F);
+        V.convertTo(V, CvType.CV_32F);
+        Core.magnitude(S, V, distance);
+        Core.inRange(distance, new Scalar(0.0), new Scalar(200.0), mThresholded2);
+        Core.bitwise_and(mThresholded, mThresholded2, mThresholded);
+        // Apply the Hough Transform to find the circles
+        Imgproc.GaussianBlur(mThresholded, mThresholded, new Size(9, 9), 0, 0);
+        Imgproc.HoughCircles(mThresholded, circles, Imgproc.CV_HOUGH_GRADIENT, 2, mThresholded.height() / 4, 500, 50, 0, 0);
+
+//        //Edge detector
+//        Imgproc.Canny(mThresholded, mThresholded, 500, 250);
+//        // It is just for display
+//        Imgproc.cvtColor(mThresholded, mRgba, Imgproc.COLOR_GRAY2RGB, 4);
+
+        int rows = circles.rows();
+        int elemSize = (int) circles.elemSize(); // Returns 12 (3 * 4bytes in a float)
+        float[] data2 = new float[rows * elemSize / 4];
+        if (data2.length > 0) {
+            circles.get(0, 0, data2); // Points to the first element and reads the whole thing into data2
+            for (int i = 0; i < data2.length; i = i + 3) {
+                Point center = new Point(data2[i], data2[i + 1]);
+                Imgproc.ellipse(mRgba, center, new Size((double) data2[i + 2], (double) data2[i + 2]), 0, 0, 360, new Scalar(255, 0, 255), 4, 8, 0);
+            }
+        }
         return mRgba;
     }
+
 
     protected void loadUI() {
 
@@ -136,10 +196,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         });
 
     }
-    
+
     /*
-    * Connecting to robot in new thread.
-    */
+     * Connecting to robot in new thread.
+     */
     private void threadConnection(final String host) {
         new Thread() {
             public void run() {
